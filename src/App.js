4c53930f6277b73d9d7db260a7a34f8183bf3db8
asw22wsa <siudevelop@gmail.com>
2022-01-15 17:08:05 +0900

    change code for study

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Dice from "./Dice";
 import HandIcon from "./HandIcon";
+import HandButton from "./HandButton";
 
 function App(){
+  const handleClick = (value) => console.log(value);
   return (
   <div>
-   <Dice />
-   <HandIcon />
+   <Dice color="blue" num={2}/>
+   <HandIcon value="rock"/>
+   <HandButton value="rock" onClick={handleClick}/>
+   <HandButton value="scissor" onClick={handleClick}/>
+   <HandButton value="paper" onClick={handleClick}/>
   </div>
   );
 }
--- src/HandIcon.js
+++ src/HandIcon.js
@@ -1,9 +1,19 @@
-import rock from './assets/rock.svg';
+import rockImg from './assets/rock.svg';
+import scissorImg from './assets/scissor.svg';
+import paperImg from './assets/paper.svg';
 
-function HandIcon(){
+const IMAGES = {
+  rock : rockImg,
+  paper : paperImg,
+  scissor : scissorImg
+}
+
+
+function HandIcon({value}){
+  const src = IMAGES[value];
   return (
     <div>
-      <img src={rock} alt="주먹"/>
+      <img src={src} alt={value}/>
     </div>
   );
 }
